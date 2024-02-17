960289122ece98c5fe497e4603be98c6d2586266
viorel.stan <viorel.stan@MP150173>
2024-02-17 12:50:37 +0200

    Add project files.

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Se citesc trei numere de la tastatura, x, y, z.
+ * Scrieti un program care va afisa cele 3 valori in ordine descrescatoare
+*/
+
+Console.WriteLine("Introduceti valoare pentru x:");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru y:");
+int y = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru z:");
+int z = int.Parse(Console.ReadLine());
+
+var arr = new[] { x, y, z };
+Array.Sort(arr);
+Array.Reverse(arr);
+Console.WriteLine("{0}, {1}, {2}",arr[0], arr[1], arr[2]);
